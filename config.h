1d2d5f6c0d6310d7d019e1fdedcca4326ad009de
Jim Calvin <jim@jced.org>
2017-09-21 15:04:48 -0400

    Update config.h
    Add new optional parameter to AdafruitIO_WiFi to put the hardware in station only mode so that an access point isn't enabled.

--- config.h
+++ config.h
@@ -20,7 +20,7 @@
 
 // comment out the following two lines if you are using fona or ethernet
 #include "AdafruitIO_WiFi.h"
-AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, WIFI_STA);
 
 
 /******************************* FONA **************************************/
